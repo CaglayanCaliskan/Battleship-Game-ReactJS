fbcd1f1c8499bedd055cbb2b3fc3a084af98086e
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-08-08 15:20:21 +0300

    test function

--- src/pages/SinglePlayer.jsx
+++ src/pages/SinglePlayer.jsx
@@ -1,20 +1,63 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const SinglePlayer = () => {
-  const boardWidth = 10;
-  const boardHeight = 10;
-  const emptyBoard = [[1], [2], [3], [4], [5], [6]];
+  const [attack, setAttack] = useState([]);
+  const emptyBoard = [];
+  const selected = [12, 14];
+
+  function starter() {
+    for (let i = 0; i < 100; i++) {
+      emptyBoard.push(i + 1);
+    }
+  }
+  starter();
+
+  //Attack function
+  const perAttack = (e) => {
+    const target = Number(e.currentTarget.dataset.coordinat);
+    setAttack([...attack, Number(e.currentTarget.dataset.coordinat)]);
+    if (selected.includes(target)) {
+      e.currentTarget.classList.add('bg-fire-bg');
+    } else {
+      e.currentTarget.classList.add('bg-blue-500');
+    }
+  };
+
   return (
     <div className='bg-brand h-screen w-full grid grid-rows-2 text-white'>
-      <div className='border flex justify-center items-center'>
-        {/* box */}
-        <div className='grid grid-cols-3 '>
+      <div className='relative border flex justify-center items-center'>
+        <div className=' absolute right-10 h-full w-48 '>
+          <h1>board:</h1>
+          <div className='border h-full relative'>
+            <div
+              onDragStart={(e) => {
+                e.dataTransfer.setData('test', e.target.dataset);
+
+                console.log(e.dataTransfer);
+              }}
+              draggable
+              className='gemi absolute flex gap-1 m-2'
+            >
+              <div className='border border-red-500 w-9 h-9'></div>
+              <div className='border border-red-500 w-9 h-9'></div>
+              <div className='border border-red-500 w-9 h-9'></div>
+            </div>
+          </div>
+        </div>
+        <div className='grid grid-cols-10  '>
           {emptyBoard.map((box) => {
-            return <div>{box}</div>;
+            return (
+              <div
+                onDragEnter={(e) => e.preventDefault()}
+                onClick={(e) => perAttack(e)}
+                data-ship='var'
+                className={`border bg-cover select-none cursor-pointer w-10 h-10 `}
+                data-coordinat={box}
+                key={box}
+              ></div>
+            );
           })}
         </div>
-
-        {/* box */}
       </div>
       <div className='border'>2</div>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
       backgroundImage: {
         'home-bg': "url('./src/assets/homebg.jpg')",
         'naval-bg': "url('./src/assets/navalbg.gif')",
+        'fire-bg': "url('./src/assets/fire.gif')",
       },
       fontFamily: {
         'main-font': ['Playfair Display SC', 'sans'],
