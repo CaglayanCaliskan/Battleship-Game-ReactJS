b3eaed34c3b7eb7f0e06e47cb1e2915b5785a3fe
Caglayan Caliskan <caliskancaglayan@gmail.com>
2022-08-08 18:58:13 +0300

    drag and drop functions

--- src/pages/MultiPlayer.jsx
+++ src/pages/MultiPlayer.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const MultiPlayer = () => {
-  return <div>MultiPlayer</div>;
+  return (
+    <main className='bg-brand h-screen select-none'>
+      <div className='flex justify-center flex-col items-center pt-4'>
+        <span className='text-white'>on Progress...</span>
+
+        <Link
+          to='/'
+          className='bg-main text-white rounded-md p-2 hover:scale-95 border border-main hover:border-white transition-all'
+        >
+          Back
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default MultiPlayer;
--- src/pages/SinglePlayer.jsx
+++ src/pages/SinglePlayer.jsx
@@ -1,107 +1,174 @@
 import React, {useEffect, useState} from 'react';
+import {MdOutlineCropRotate} from 'react-icons/md';
 
 //5 diffrent size ships with id
 // dragable ships move to board
 
 const SinglePlayer = () => {
-  const [attack, setAttack] = useState([]);
-  const emptyBoard = [];
-  const selected = [12, 14];
+  const [emptyBoard, setEmptyBoard] = useState([]);
+  const [shipsRotate, setShipsRotate] = useState(false);
+  const [selected, setSelected] = useState();
 
-  function starter() {
+  useEffect(() => {
+    createBoard();
+  }, []);
+
+  const createBoard = () => {
+    let arr = [];
     for (let i = 0; i < 100; i++) {
-      emptyBoard.push(i + 1);
+      arr.push(i + 1);
     }
-  }
-  starter();
-
-  //Attack function
-  const perAttack = (e) => {
-    const target = Number(e.currentTarget.dataset.coordinat);
-    setAttack([...attack, Number(e.currentTarget.dataset.coordinat)]);
-    if (selected.includes(target)) {
-      e.currentTarget.classList.add('bg-fire-bg');
-    } else {
-      e.currentTarget.classList.add('bg-blue-500');
-    }
+    setEmptyBoard(arr);
   };
 
-  //Ship Factory
+  // DRAG and DROP FUNCTİONS
+
+  const setShipSelect = (e) => {
+    setSelected(e.currentTarget);
+  };
+
+  const shipEnter = (e) => {
+    e.currentTarget.classList.add('bg-green-500');
+    console.log(selected);
+  };
+  const shipLeave = (e) => {
+    e.currentTarget.classList.remove('bg-green-500');
+  };
 
   return (
-    <div className='bg-brand h-screen w-full grid grid-rows-2 text-white'>
-      <div className='relative border flex justify-center items-center'>
-        <div className=' absolute right-10 h-5/6 '>
-          <h1>Your Fleet:</h1>
-
-          <div className='fleet border flex flex-col'>
-            <div
-              draggable
-              id='destroyer'
-              className='flex gap-1 m-2 w-fit relative'
-            >
-              <div className='border border-yellow-500 w-9 h-9'></div>
-              <div className='border border-yellow-500 w-9 h-9'></div>
-            </div>
-            <div
-              id='submarine'
-              draggable
-              className='flex gap-1 m-2 w-fit relative'
-            >
-              <div className='border border-yellow-500 w-9 h-9'></div>
-              <div className='border border-yellow-500 w-9 h-9'></div>
-              <div className='border border-yellow-500 w-9 h-9'></div>
-            </div>
-            <div
-              id='cruiser'
-              draggable
-              className='flex gap-1 m-2 w-fit relative'
-            >
-              <div className='border border-yellow-500 w-9 h-9'></div>
-              <div className='border border-yellow-500 w-9 h-9'></div>
-              <div className='border border-yellow-500 w-9 h-9'></div>
-            </div>
-            <div
-              id='battleship'
-              draggable
-              className='flex gap-1 m-2 w-fit relative'
-            >
-              <div className='border border-yellow-500 w-9 h-9'></div>
-              <div className='border border-yellow-500 w-9 h-9'></div>
-              <div className='border border-yellow-500 w-9 h-9'></div>
-              <div className='border border-yellow-500 w-9 h-9'></div>
-            </div>
-            <div
-              id='carrier'
-              draggable
-              className='flex gap-1 m-2 w-fit relative'
-            >
-              <div className='border border-yellow-500 w-9 h-9'></div>
-              <div className='border border-yellow-500 w-9 h-9'></div>
-              <div className='border border-yellow-500 w-9 h-9'></div>
-              <div className='border border-yellow-500 w-9 h-9'></div>
-              <div className='border border-yellow-500 w-9 h-9'></div>
-            </div>
+    <main className='bg-brand h-screen select-none'>
+      <div className='flex justify-around pt-4'>
+        <div id='your-board' className='text-center'>
+          <div className='bg-green-300 rounded mb-4'>
+            <h1>Your Board</h1>
+          </div>
+          <div className='grid grid-cols-10 gap-1 '>
+            {emptyBoard &&
+              emptyBoard.map((box) => {
+                return (
+                  <div
+                    onDragEnter={shipEnter}
+                    onDragLeave={shipLeave}
+                    data-ship='var'
+                    className={`border bg-blue-200 hover:bg-blue-300 bg-cover select-none  w-8 h-8`}
+                    data-coordinat={box}
+                    key={box}
+                    id={box}
+                  ></div>
+                );
+              })}
           </div>
         </div>
-        <div className='grid grid-cols-10  '>
-          {emptyBoard.map((box) => {
-            return (
-              <div
-                onDragEnter={(e) => e.preventDefault()}
-                onClick={(e) => perAttack(e)}
-                data-ship='var'
-                className={`border bg-cover select-none cursor-pointer w-10 h-10 `}
-                data-coordinat={box}
-                key={box}
-                id={box}
-              ></div>
-            );
-          })}
+        <div id='computer-board' className='text-center'>
+          <div className='bg-red-300 rounded mb-4'>
+            <h1>Computer Board</h1>
+          </div>
+          <div className='grid grid-cols-10 gap-1 '>
+            {emptyBoard &&
+              emptyBoard.map((box) => {
+                return (
+                  <div
+                    data-ship='var'
+                    className={`border bg-cover select-none cursor-pointer w-8 h-8`}
+                    data-coordinat={box}
+                    key={box}
+                    id={box}
+                  ></div>
+                );
+              })}
+          </div>
         </div>
       </div>
-      <div className='border'>2</div>
-    </div>
+      <div className=' mt-5 flex flex-col items-center'>
+        <h1 className='text-white text-center text-xl select-none'>
+          Locate Your Ships...
+        </h1>
+        <div
+          onClick={() => setShipsRotate(!shipsRotate)}
+          className='bg-red-500 rounded-full text-white p-1 cursor-pointer active:rotate-90 hover:bg-white hover:text-red-500'
+        >
+          <MdOutlineCropRotate
+            size={30}
+            className=' rounded flex items-center justify-center'
+          />
+        </div>
+
+        <div className={` flex justify-center`}>
+          <div
+            draggable
+            id='test'
+            className={`${
+              shipsRotate ? '' : 'flex-col'
+            } flex gap-1 m-2 w-fit relative h-fit`}
+            onDragStart={setShipSelect}
+          >
+            <div className='border border-yellow-500 w-8 h-8'></div>
+          </div>
+          <div
+            draggable
+            id='destroyer'
+            className={`${
+              shipsRotate ? '' : 'flex-col'
+            } flex gap-1 m-2 w-fit relative h-fit`}
+            onDragStart={setShipSelect}
+          >
+            <div className='border border-yellow-500 w-8 h-8'></div>
+            <div className='border border-yellow-500 w-8 h-8'></div>
+          </div>
+          <div
+            id='submarine'
+            draggable
+            className={`${
+              shipsRotate ? '' : 'flex-col'
+            } flex gap-1 m-2 w-fit relative h-fit`}
+            onDragStart={setShipSelect}
+          >
+            <div className='border border-yellow-500 w-8 h-8'></div>
+            <div className='border border-yellow-500 w-8 h-8'></div>
+            <div className='border border-yellow-500 w-8 h-8'></div>
+          </div>
+          <div
+            id='cruiser'
+            draggable
+            className={`${
+              shipsRotate ? '' : 'flex-col'
+            } flex gap-1 m-2 w-fit relative h-fit`}
+            onDragStart={setShipSelect}
+          >
+            <div className='border border-yellow-500 w-8 h-8'></div>
+            <div className='border border-yellow-500 w-8 h-8'></div>
+            <div className='border border-yellow-500 w-8 h-8'></div>
+          </div>
+          <div
+            id='battleship'
+            draggable
+            className={`${
+              shipsRotate ? '' : 'flex-col'
+            } flex gap-1 m-2 w-fit relative h-fit`}
+            onDragStart={setShipSelect}
+          >
+            <div className='border border-yellow-500 w-8 h-8'></div>
+            <div className='border border-yellow-500 w-8 h-8'></div>
+            <div className='border border-yellow-500 w-8 h-8'></div>
+            <div className='border border-yellow-500 w-8 h-8'></div>
+          </div>
+          <div
+            id='carrier'
+            draggable
+            className={`${
+              shipsRotate ? '' : 'flex-col'
+            } flex gap-1 m-2 w-fit relative h-fit`}
+            onDragStart={setShipSelect}
+          >
+            <div className='border border-yellow-500 w-8 h-8'></div>
+            <div className='border border-yellow-500 w-8 h-8'></div>
+            <div className='border border-yellow-500 w-8 h-8'></div>
+            <div className='border border-yellow-500 w-8 h-8'></div>
+            <div className='border border-yellow-500 w-8 h-8'></div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
